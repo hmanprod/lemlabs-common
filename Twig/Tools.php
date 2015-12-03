9903edc87aea63a7d206e660a5834c3131ddbb40
Hery <hmanprod@gmail.com>
2015-12-03 18:04:42 +0300

    Fix Unit filter method

--- Twig/Tools.php
+++ Twig/Tools.php
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class Tools extends \Twig_Extension
 {
 	protected $container;
-	
+
 	public function __construct(ContainerInterface $serviceContainer)
 	{
 		$this->container = $serviceContainer;
@@ -24,35 +24,37 @@ class Tools extends \Twig_Extension
         	'plural' => new \Twig_Filter_Method($this, 'plural'),
         );
     }
-	
+
 	public function getTests()
     {
     	return array(
     	'getControllerName' =>  new \Twig_Function_Method($this, 'getControllerName')
     	);
     }
-    
+
     public function unit($data, $unit, $na = '-'){
-    	if($data > 0)
-    		return $data.' '.$unit;
-    	return $na;
+			if(!$data)
+				return $na;
+    	return $data.' '.$unit;
     }
-    
+
     public function plural($data){
     	if((int)$data > 1)
     		return 's';
     }
     
-   
+    
+
+
     public function getControllerName()
     {
 		$controller = $container->getRequest()->attributes->get('_controller');
 		preg_match('/(.*)\\\Bundle\\\(.*)\\\Controller\\\(.*)Controller::(.*)Action/', $controller, $matches);
-		
+
 		if(isset($matches[3]) && $matches[3])
 			return $matches[3];
     }
-    
+
     public function getName()
     {
         return 'tools_extension';
